24ee40a45c3ad1746673a30c49fce0c2e5b9ab93
cypl <cyrille.plenchette@me.com>
2023-05-11 16:17:49 +0200

    add JS Doc HeadUser component

--- src/components/HeadUser.js
+++ src/components/HeadUser.js
@@ -3,32 +3,16 @@ import PropTypes from 'prop-types'
 import SwitchData from './SwitchData'
 import Loader from './Loader'
 
-const HeadingUser = styled.header`
-  position: absolute;
-  width: 90%;
-  max-width: 1100px;
-  left: 50%;
-  transform: translate(-50%, 0);
-  height: 150px;
-  @media (max-width: 1024px) {
-    width: calc(100% - 60px);
-  }
-`
-const HelloUser = styled.h1`
-  font-size: 44px;
-  line-height: 1;
-  font-weight: 500;
-  margin-top: 35px;
-`
-const MessageUser = styled.p`
-  line-height: 1.4;
-  font-weight: 400;
-  margin-top: 12px;
-`
-const UserFirstName = styled.span`
-  color: #e60000;
-`
-
+/**
+ *
+ * @param {object} props - The props object containing the following properties:
+ * @param {object} props.userData - The object containing the user data.
+ * @param {string} props.dataSource - The name of the data source.
+ * @param {function} props.switchUser - The function to switch user.
+ * @param {function} props.switchMockSource - The function to switch to mock data source.
+ * @param {function} props.switchDevSource - The function to switch to development data source.
+ * @returns {JSX.Element} - The JSX markup for the HeadUser component.
+ */
 function HeadUser({
   userData,
   dataSource,
@@ -70,3 +54,29 @@ HeadUser.propTypes = {
   switchMockSource: PropTypes.func,
   switchDevSource: PropTypes.func,
 }
+
+const HeadingUser = styled.header`
+  position: absolute;
+  width: 90%;
+  max-width: 1100px;
+  left: 50%;
+  transform: translate(-50%, 0);
+  height: 150px;
+  @media (max-width: 1024px) {
+    width: calc(100% - 60px);
+  }
+`
+const HelloUser = styled.h1`
+  font-size: 44px;
+  line-height: 1;
+  font-weight: 500;
+  margin-top: 35px;
+`
+const MessageUser = styled.p`
+  line-height: 1.4;
+  font-weight: 400;
+  margin-top: 12px;
+`
+const UserFirstName = styled.span`
+  color: #e60000;
+`
