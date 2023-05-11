199d0726f24eb9d6dc3978a2319ba294c60c2116
cypl <cyrille.plenchette@me.com>
2023-05-11 16:28:15 +0200

    add JS Doc BtnTxt component

--- src/components/BtnText.js
+++ src/components/BtnText.js
@@ -2,6 +2,24 @@ import styled from 'styled-components'
 import PropTypes from 'prop-types'
 import { NavLink } from 'react-router-dom'
 
+/**
+ * Display a button as an inline text.
+ * @param {object} props - The props object containing the following properties:
+ * @param {string}  props.text - The text of the button.
+ * @param {string}  props.target - The target (route) of the button.
+ * @returns {JSX.Element} - The JSX markup for the BtnTxt component.
+ */
+function BtnTxt({ text, target }) {
+  return <Btn to={target}>{text}</Btn>
+}
+
+export default BtnTxt
+
+BtnTxt.propTypes = {
+  text: PropTypes.string,
+  target: PropTypes.string,
+}
+
 const Btn = styled(NavLink)`
   display: inline-block;
   padding: 15px 20px;
@@ -21,14 +39,3 @@ const Btn = styled(NavLink)`
     transition: 0.05s color ease-in-out, 0.1s background-color ease-in-out;
   }
 `
-
-function BtnTxt({ text, target }) {
-  return <Btn to={target}>{text}</Btn>
-}
-
-export default BtnTxt
-
-BtnTxt.propTypes = {
-  text: PropTypes.string,
-  target: PropTypes.string,
-}
