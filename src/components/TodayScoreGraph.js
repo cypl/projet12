831bfd325cf0e543c2eca7e182580cdeb9d7f056
cypl <cyrille.plenchette@me.com>
2023-05-11 15:16:30 +0200

    add JS Doc TodayScoreGraph component

--- src/components/TodayScoreGraph.js
+++ src/components/TodayScoreGraph.js
@@ -3,24 +3,18 @@ import PropTypes from 'prop-types'
 import Loader from './Loader'
 import { PieChart, Pie, Cell, ResponsiveContainer } from 'recharts'
 
-const ScoreWrapperMargin = styled.div`
-  position: absolute;
-  top: 15px;
-  left: 25px;
-  height: calc(100% - 30px);
-  width: calc(100% - 50px);
-`
-const TitleGraph = styled.h3`
-  line-height: 24px;
-  font-size: 15px;
-  font-weight: 500;
-  position: absolute;
-  left: 10px;
-  top: 0;
-  color: #222;
-`
-
+/**
+ * Displays the Today Score graph.
+ * @param {object} props - The props object containing the following properties:
+ * @param {object}  props.userData - The object containing the user data.
+ * @returns {JSX.Element} - The JSX markup for the TodayScoreGraph component.
+ */
 function TodayScoreGraph({ userData }) {
+  /**
+   * Data needs to be re-formated to be used as graph data.
+   * @param {object} data - The object containing the user data.
+   * @returns {object} - The object containing data used for the graph.
+   */
   function formatTodayScore(data) {
     if (data.id !== undefined) {
       const score = [
@@ -38,18 +32,18 @@ function TodayScoreGraph({ userData }) {
       {userData.isDataLoading ? (
         <Loader size={'28px'} />
       ) : (
-        <ResponsiveContainer width='100%' height='100%'>
+        <ResponsiveContainer width="100%" height="100%">
           <PieChart width={400} height={400}>
             <Pie
               data={dataScore}
-              dataKey='todayScore'
+              dataKey="todayScore"
               startAngle={90}
               endAngle={450}
-              cx='50%'
-              cy='50%'
+              cx="50%"
+              cy="50%"
               innerRadius={70}
               outerRadius={80}
-              stroke='#fbfbfb'
+              stroke="#fbfbfb"
               cornerRadius={50}
               animationBegin={0}
               animationDuration={400}
@@ -57,21 +51,21 @@ function TodayScoreGraph({ userData }) {
               <Cell fill={'#e60000'} />
               <Cell fill={'#fbfbfb'} />
             </Pie>
-            <circle cx='50%' cy='50%' r='70' fill='#fff' />
-            <text textAnchor='middle' fontSize={15} fontWeight={400}>
+            <circle cx="50%" cy="50%" r="70" fill="#fff" />
+            <text textAnchor="middle" fontSize={15} fontWeight={400}>
               <tspan
-                x='50%'
-                y='46%'
+                x="50%"
+                y="46%"
                 fontSize={26}
                 fill={'#282D30'}
                 fontWeight={700}
               >
                 {dataScore && dataScore[0].todayScore * 100}%
               </tspan>
-              <tspan x='50%' y='56%' fill={'#9b9eac'}>
+              <tspan x="50%" y="56%" fill={'#9b9eac'}>
                 de votre
               </tspan>
-              <tspan x='50%' y='65%' fill={'#9b9eac'}>
+              <tspan x="50%" y="65%" fill={'#9b9eac'}>
                 objectif
               </tspan>
             </text>
@@ -87,3 +81,20 @@ export default TodayScoreGraph
 TodayScoreGraph.propTypes = {
   userData: PropTypes.object,
 }
+
+const ScoreWrapperMargin = styled.div`
+  position: absolute;
+  top: 15px;
+  left: 25px;
+  height: calc(100% - 30px);
+  width: calc(100% - 50px);
+`
+const TitleGraph = styled.h3`
+  line-height: 24px;
+  font-size: 15px;
+  font-weight: 500;
+  position: absolute;
+  left: 10px;
+  top: 0;
+  color: #222;
+`
