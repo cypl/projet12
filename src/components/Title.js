b64ae1c629aabe86e583d1d96c47032ad2447bc1
cypl <cyrille.plenchette@me.com>
2023-05-11 15:22:13 +0200

    add JS Doc Title component

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,16 +1,12 @@
 import styled from 'styled-components'
 import PropTypes from 'prop-types'
 
-const PageTitle = styled.h1`
-  text-align: center;
-  font-size: 70px;
-  letter-spacing: -3px;
-  line-height: 1;
-  padding-bottom: 20px;
-  font-weight: 400;
-  color: #e60000;
-`
-
+/**
+ * Displays a title
+ * @param {object} props - The props object containing the following properties:
+ * @param {string} props.text - The text of the Title component.
+ * @returns {JSX.Element} - The JSX markup for the Title component.
+ */
 function Title({ text }) {
   return <PageTitle>{text}</PageTitle>
 }
@@ -20,3 +16,13 @@ export default Title
 Title.propTypes = {
   text: PropTypes.string,
 }
+
+const PageTitle = styled.h1`
+  text-align: center;
+  font-size: 70px;
+  letter-spacing: -3px;
+  line-height: 1;
+  padding-bottom: 20px;
+  font-weight: 400;
+  color: #e60000;
+`
