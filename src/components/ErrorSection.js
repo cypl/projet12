f0cb8e97d499e2cd1b7f9a1c9acaeebd37cc1f29
cypl <cyrille.plenchette@me.com>
2023-05-11 16:25:02 +0200

    add JS Doc ErrorSection component

--- src/components/ErrorSection.js
+++ src/components/ErrorSection.js
@@ -3,28 +3,15 @@ import PropTypes from 'prop-types'
 import Title from './Title'
 import BtnTxt from './BtnText'
 
-const Main = styled.main`
-  position: absolute;
-  top: 90px;
-  left: 110px;
-  width: calc(100% - 110px);
-  height: calc(100% - 90px);
-`
-const ErrorSectionWrapper = styled.section`
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`
-const ErrorContainer = styled.article`
-  width: 95%;
-`
-const ErrorText = styled.p`
-  text-align: center;
-`
-
+/**
+ * Displays the section for Error404 or InProgress pages.
+ * @param {object} props - The props object containing the following properties:
+ * @param {string}  props.title - The title of the error.
+ * @param {string}  props.errorText - The text describing the error.
+ * @param {string}  props.btnText - The text of the button.
+ * @param {string}  props.btnTarget - The target (route) of the button.
+ * * @returns {JSX.Element} - The JSX markup for the ErrorSection component.
+ */
 function ErrorSection({ title, errorText, btnText, btnTarget }) {
   return (
     <Main>
@@ -49,3 +36,25 @@ ErrorSection.propTypes = {
   btnText: PropTypes.string,
   btnTarget: PropTypes.string,
 }
+
+const Main = styled.main`
+  position: absolute;
+  top: 90px;
+  left: 110px;
+  width: calc(100% - 110px);
+  height: calc(100% - 90px);
+`
+const ErrorSectionWrapper = styled.section`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+const ErrorContainer = styled.article`
+  width: 95%;
+`
+const ErrorText = styled.p`
+  text-align: center;
+`
